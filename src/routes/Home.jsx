1af01752f5b38925ecfbdea679a0000641a0125e
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-02-08 13:19:14 -0400

    I created the home page and styled it

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,24 @@
+import './Home.modules.css';
+
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="home">
+      <h2>Monte a sua contagem regressiva!</h2>
+      <form className="countDownForm">
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            placeholder="Digite o nome do evento"
+          />
+        </label>
+        <label>
+          <span>Data do evento:</span>
+          <input type="date" name="date" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
   );
 };
